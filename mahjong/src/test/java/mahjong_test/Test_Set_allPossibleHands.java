491bbd527cb1d3dc8f8e76c8d7e8487cd316fdef
JackalCheung <44915725+JackalCheung@users.noreply.github.com>
2018-12-10 22:59:22 +0800

    Update Test_Set_allPossibleHands.java

--- mahjong/src/test/java/mahjong_test/Test_Set_allPossibleHands.java
+++ mahjong/src/test/java/mahjong_test/Test_Set_allPossibleHands.java
@@ -8,6 +8,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import mahjong.Game;
+import mahjong.InvalidTileException;
+import mahjong.Set;
+import mahjong.Tile;
+
 public class Test_Set_allPossibleHands {
 
 	@Before
